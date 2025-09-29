e74d5b0e4eb83ca50ff57aa4adec3206de637900
Svetlana Dorado <dorado.svetlana@gmail.com>
2025-09-29 21:00:04 +0200

    Update formatting in homework_4

--- homework/svetlana_dorado/homework_4/homework_4.py
+++ homework/svetlana_dorado/homework_4/homework_4.py
@@ -1,28 +1,28 @@
 my_dict = {
-"tuple": (
-    'uno',
-    'dos',
-    'tres',
-    'cuatro',
-    'cinco'
-),
-"list": [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five'
-],
-"dict": {
-    'key_one': 'value_one',
-    'key_two': 'value_two',
-    'key_three': 'value_three',
-    'key_four': 'value_four',
-    'key_five': 'value_five'
-},
-"set" : {
-    1, 2, 3, 4, 5
-}
+    "tuple": (
+        'uno',
+        'dos',
+        'tres',
+        'cuatro',
+        'cinco'
+    ),
+    "list": [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five'
+    ],
+    "dict": {
+        'key_one': 'value_one',
+        'key_two': 'value_two',
+        'key_three': 'value_three',
+        'key_four': 'value_four',
+        'key_five': 'value_five'
+    },
+    "set": {
+        1, 2, 3, 4, 5
+    }
 }
 
 print(my_dict["tuple"][-1])
