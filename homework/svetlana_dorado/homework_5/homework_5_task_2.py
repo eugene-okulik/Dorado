6d898952f46772486f0b64cea2de83ee7c6401ae
Svetlana Dorado <dorado.svetlana@gmail.com>
2025-10-05 17:22:21 +0200

    Add homework_5: task_1, task_2, task_3

---
+++ homework/svetlana_dorado/homework_5/homework_5_task_2.py
@@ -0,0 +1,22 @@
+operation_result_1 = "результат операции: 42"
+result_index = operation_result_1.index(":")
+number_start = operation_result_1[result_index + 2:]
+number = int(number_start)
+result = number + 10
+print(result)
+
+
+operation_result_2 = "результат операции: 514"
+result_index = operation_result_2.index(":")
+number_start = operation_result_2[result_index + 2:]
+number = int(number_start)
+result = number + 10
+print(result)
+
+
+program_result = "результат работы программы: 9"
+result_index = program_result.index(":")
+number_start = program_result[result_index + 2:]
+number = int(number_start)
+result = number + 10
+print(result)
---
+++ homework/svetlana_dorado/homework_5/homework_5_task_3.py
@@ -0,0 +1,7 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+name_1, name_2, name_3 = students
+subject_1, subject_2, subject_3 = subjects
+
+print(f"Students {name_1}, {name_2}, {name_3} study these subjects: {subject_1}, {subject_2}, {subject_3}")
