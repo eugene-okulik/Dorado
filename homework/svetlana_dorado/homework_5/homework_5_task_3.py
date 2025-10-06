1ea7bb1ad6f31a23b01777ffd3debf9af920bdd5
Svetlana Dorado <dorado.svetlana@gmail.com>
2025-10-06 23:09:04 +0200

    Update homework_5_task_3 using the join() function

--- homework/svetlana_dorado/homework_5/homework_5_task_3.py
+++ homework/svetlana_dorado/homework_5/homework_5_task_3.py
@@ -1,7 +1,4 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-name_1, name_2, name_3 = students
-subject_1, subject_2, subject_3 = subjects
-
-print(f"Students {name_1}, {name_2}, {name_3} study these subjects: {subject_1}, {subject_2}, {subject_3}")
+print(f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}")
